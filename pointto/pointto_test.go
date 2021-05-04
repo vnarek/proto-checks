35f4e4311f0560edb4e27240b8bbd7b20c35e104
Václav Král <kralva10@fit.cvut.cz>
2021-05-04 23:36:47 +0200

    Added some notes and new test case

--- pointto/pointto_test.go
+++ pointto/pointto_test.go
@@ -80,3 +80,47 @@ func main() {
 
 	t.Logf("%#v\n", sol)
 }
+
+//source: http://pages.cs.wisc.edu/~fischer/cs701.f08/lectures/Lecture26.4up.pdf
+func TestWiscCase(t *testing.T) {
+	tt := `
+package main
+
+func main() {
+	p1 = &a;
+	p2 = &b;
+	p1 = p2;
+	r = &p1;
+	*r = &c
+	p3 = *r;
+	p2 = &d;
+}`
+	// Create the AST by parsing src.
+	fset := token.NewFileSet() // positions are relative to fset
+	f, err := parser.ParseFile(fset, "", tt, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	funDecl, ok := f.Decls[0].(*ast.FuncDecl)
+	if !ok {
+		panic("not funDecl")
+	}
+
+	c := cfg.New(
+		funDecl.Body,
+		func(ce *ast.CallExpr) bool { return true },
+	)
+	b := normalizeCfg.NewBuilder()
+	b.Build(c.Blocks[0])
+
+	sol := Build(b)
+
+	/* Should return:
+	 * p1 = a, b, c, d
+	 * p2 = b, d
+	 * p3 = a, b, c, d
+	 * r = p1
+	 */
+	t.Logf("%#v\n", sol)
+}
