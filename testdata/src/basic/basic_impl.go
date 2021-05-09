5d49e29a835aab0eea870aa2f50a1f1135351ee0
Václav Král <kralva10@fit.cvut.cz>
2021-05-09 17:45:52 +0200

    Added some tests

--- testdata/src/basic/basic_impl.go
+++ testdata/src/basic/basic_impl.go
@@ -3,6 +3,7 @@ package basic
 import (
 	"context"
 	"fmt"
+	"math/rand"
 )
 
 type Handler struct {
@@ -20,4 +21,42 @@ func (h *Handler) HiHello(ctx context.Context, a *int, b *bool) {
 	d := *c // want "c could be nil"
 
 	fmt.Sprintln(c, d)
+}
+
+func (h *Handler) Rng() {
+	def := new(int)
+	x := def
+	if *x == rand.Int() {
+		x = nil
+	} else {
+		*x = 42
+	}
+	y := *x // want "x could be nil"
+	fmt.Sprint(y)
+}
+
+func (h *Handler) NotNil() {
+	var x *int
+	if rand.Int() == 0 {
+		x = new(int)
+	} else {
+		x = new(int)
+	}
+	fmt.Sprint(*x)
+}
+
+func (h *Handler) SwitchRng() {
+	x := new(int)
+	*x = rand.Int()
+	switch *x {
+	case 0:
+		*x = *x + 1
+	case 1:
+		x = nil
+	case 2:
+		*x = *x - 1
+	default:
+		*x = *x / *x
+	}
+	*x = 0 // want "x could be nil"
 }
